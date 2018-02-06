76c2a848f8369dcbcc299780baa0e72578fc2e11
Zhen Li <leezhen1988@gmail.com>
2018-02-06 22:43:41 +0800

    Update Function.3min.cal.R

--- Function.3min.cal.R
+++ Function.3min.cal.R
@@ -235,3 +235,123 @@ cal_3min<- function(filename,unique_mass,mz_window = 0.01,RT_width = 20,span = 0
   expr_not_mapped<- expr_not_mapped[!is.na(expr_not_mapped[,2]),]
   return(list(expr_mapped,expr_not_mapped))
 }
+
+# using multiple mz window to calculate
+cal_3min_multi_mz_window<- function(filename,unique_mass,mz_window = 0.01,RT_width = 20,span = 0.05,SNR.th = 3,hash_RT,polarity){
+  ms<- openMSfile(filename)
+  hd<- header(ms)
+  peaks<- peaks(ms)
+  peaks<- lapply(peaks,function(x){
+    return(t(x[x[,2]!=0,]))
+  })
+  length_peak<- unlist(lapply(peaks,function(x){return(dim(x)[2])}))
+  peak_all<- matrix(unlist(peaks),ncol = 2,byrow = T)
+  rt<- sapply(1:length(length_peak),function(x){return(rep(hd$retentionTime[x],length_peak[x]))})
+  rt<- unlist(rt)
+  peak_all<- cbind(peak_all,rt)
+  peak_all<- peak_all[!is.na(peak_all[,1]),]
+  colnames(peak_all)<- c("mz","intensity","RT")
+  index<- floor(peak_all[,1])
+  key<- unique(index)
+  h<- hash()
+  for (i in 1:length(key)){
+    .set(h, keys = key[i], values = peak_all[index==key[i],])
+  }
+  for (i in 1:length(mz_window)){
+    tmp<- sapply(unique_mass,function(x){
+    #tmp<- sapply(exact_mass[1:2],function(x){
+    lowmz = x-mz_window[i]+polarity*1.007276
+    highmz = x+mz_window[i]+polarity*1.007276
+    #chromatogram_raw<- peak_all[peak_all[,1]>= lowmz & peak_all[,1] <= highmz,c(3,2)]
+    if (floor(lowmz)!=floor(highmz)){
+      peak_sub<- rbind(h[[as.character(floor(lowmz))]],h[[as.character(floor(highmz))]])
+    } else {
+      peak_sub<- h[[as.character(floor(lowmz))]]
+    }
+    if (class(peak_sub)=="numeric"){
+      chromatogram_raw<- c(0,0)
+    } else{
+      chromatogram_raw<- peak_sub[peak_sub[,1]>= lowmz & peak_sub[,1] <= highmz,c(3,2)]
+    }
+    if (class(chromatogram_raw)=="numeric" | class(chromatogram_raw)=="NULL"){
+      chromatogram_mat<- list("NULL")
+      #return(chromatogram_mat)
+    } else {
+      if (dim(chromatogram_raw)[1]<3){
+        chromatogram_mat<- list("NULL")
+        #return(chromatogram_mat)
+      } else {
+        unique_rt<- unique(chromatogram_raw[,1])
+        intensity<- sapply(unique_rt,function(x){
+          return(sum(chromatogram_raw[chromatogram_raw[,1]==x,2]))
+        })
+        chromatogram_mat<- cbind(unique_rt,intensity)
+        colnames(chromatogram_mat)<- c("RT","intensity")
+        #average_level<- mean(chromatogram_mat[order(chromatogram_mat[,2])[(1+floor(dim(chromatogram_mat)[1]/10)):(dim(chromatogram_mat)[1]-floor(dim(chromatogram_mat)[1]/10))],2])
+        #return(chromatogram_mat)
+      }
+    }
+    if (class(chromatogram_mat)=="list"){
+      mass<- NA
+      RT_calculated<- NA
+      peak_area<- NA
+    } else {
+      chromatogram_full<- chromatogram_fill(hd,chromatogram_mat)
+      chromatogram_peak<- peak_find(chromatogram_full[,1],chromatogram_full[,2],width = RT_width, span = span,SNR.th = SNR.th)
+      #plot chromatogram
+      #plot_chromatogram(paste("Chromatogram",cpd[k,2],sub_folder_list[j],"png",sep = "."),chromatogram_mat[,1],chromatogram_mat[,2],RT_width, span = span, RT_peak = chromatogram_peak)
+      #plot_chromatogram("test.png",chromatogram_full[,1],chromatogram_full[,2],RT_width, span = span, RT_peak = chromatogram_peak)
+      #plot_chromatogram(paste(folder_list,cpd_group[j],x,"png",sep = "."),chromatogram_full[,1],chromatogram_full[,2],RT_width, span = span, RT_peak = chromatogram_peak)
+      RT_calculated<- chromatogram_peak$RT
+      if (length(RT_calculated)>0){
+        peak_area<- peak_area_cal(chromatogram_mat,RT_calculated)
+        mass<- rep(x,length(RT_calculated))
+      } else {
+        mass<- NA
+        RT_calculated<- NA
+        peak_area<- NA
+      }
+    }
+    index_cpd<- hash_RT[[as.character(x)]]
+    RT_map_res<- RT_match(index_cpd,RT_calculated,peak_area,x)
+
+    #res<- cbind(mass,RT_calculated,peak_area)
+    return(RT_map_res)
+  })
+    expr_mapped<- matrix(unlist(tmp[1,]),ncol = 2, byrow = T)
+    colnames(expr_mapped)<- paste(c("RT","Area"),mz_window[i],sep = "_")
+    if (i==1){
+      res_mat<- expr_mapped
+    } else{
+      res_mat<- cbind(res_mat,expr_mapped)
+    }
+  }
+  return(res_mat)
+}
+
+Cal_by_directory<- function(directory,mz_window,row_head,hash_RT,unique_mass,polarity,RT_width){
+  filelist<- dir(directory)
+  filelist<- filelist[grepl("mzXML",filelist)]
+  filelist<- filelist[!grepl("Blank",filelist)]
+  res_mapped_list_tmp<- list()
+  #res_not_mapped_list_tmp<- list()
+  for (j in 1:length(filelist)){
+    print(j)
+    res_3min<- cal_3min_multi_mz_window(paste(directory,filelist[j],sep = "/"),unique_mass = unique_mass,mz_window = mz_window,hash_RT = hash_RT,polarity = -1)
+    colnames(res_3min)<- paste(colnames(res_3min),substr(filelist[j],1,nchar(filelist)-6),sep = "_")
+    res_mapped_list_tmp[[j]]<- res_3min[[1]]
+  }
+  res_mapped<- Reduce(cbind,res_mapped_list_tmp)
+  res_mapped<- data.frame(res_mapped)
+  #colnames(res_mapped)[seq(1,(2*length(filelist)),2)] <- paste("RT",substr(filelist,1,nchar(filelist)-6),sep = "_")
+  #colnames(res_mapped)[seq(2,(2*length(filelist)),2)] <- paste("Area",substr(filelist,1,nchar(filelist)-6),sep = "_")
+  res_mapped<- cbind(row_head,res_mapped)
+  
+  #res_unmapped<- Reduce(rbind,res_not_mapped_list_tmp)
+  #unmapped_length<- unlist(lapply(res_not_mapped_list_tmp,function(x){return(dim(x)[1])}))
+  #file_tag<- sapply(1:length(unmapped_length),function(x){return(rep(filelist[x],unmapped_length[x]))})
+  #res_unmapped<- cbind(file_tag,res_unmapped)
+  #colnames(res_unmapped)<- c("sample","mass","RT","area")
+  return(res_mapped)
+  #return(list(res_mapped,res_unmapped))
+}
